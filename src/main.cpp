ac15fe77729470eabacee4900c71c814fcb2fcec
Benoit <benoit@milk-vfx.com>
2015-03-17 18:51:06 +0000

    Proper use of OptionParser

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 int main(int argc, char**argv)
 {
-    const std::string usage = "usage: %prog [OPTION]... DIR [FILE]...";
+    const std::string usage = "usage: %prog [OPTION]... [FILE]...";
     const std::string version = "%prog 1.0\nCopyright (C) 2015 Benoit Leveau\n";
     
     optparse::OptionParser parser = optparse::OptionParser()
@@ -17,17 +17,17 @@ int main(int argc, char**argv)
 
     parser.add_option("--interactive").action("store_true").help("Interactive mode");
 
-    optparse::OptionGroup group = optparse::OptionGroup(parser, "Global Options", "Sets the image size.");
-    group.add_option("-w", "--width").action("store").type("int").set_default(640).help("default: %default");
-    group.add_option("-h", "--height").action("store").type("int").set_default(480).help("default: %default");
-    group.add_option("-t", "--threads").action("store").type("int").set_default(8).help("Sets the number of threads. default: %default");
+    optparse::OptionGroup group = optparse::OptionGroup(parser, "Global Options");
+    group.add_option("-w", "--width").action("store").type("int").set_default(640).help("Output width. (default: %default)");
+    group.add_option("-h", "--height").action("store").type("int").set_default(480).help("Output height. (default: %default)");
+    group.add_option("-t", "--threads").action("store").type("int").set_default(0).help("Sets the number of threads. (default: %default - use all available cores)");
     parser.add_option_group(group);
     
-    optparse::OptionGroup group_tiled = optparse::OptionGroup(parser, "Tiled Renderer Options", "Options for the Tiled Renderer mode.");
+    optparse::OptionGroup group_tiled = optparse::OptionGroup(parser, "Tiled Renderer Options");
     group_tiled.add_option("--show-window").action("store_true").help("Show a render window.");
-    group_tiled.add_option("--bucket-size").action("int").set_default(64).help("Sets the bucket size. Defaults to %default.");
+    group_tiled.add_option("--bucket-size").action("int").set_default(64).help("Sets the bucket size. default: %default");
     char const* const modes[] = { "spiral", "topleft" };
-    group_tiled.add_option("-m", "--mode").choices(&modes[0], &modes[1]).set_default("spiral").help("Sets the image mode.");
+    group_tiled.add_option("-m", "--mode").choices(&modes[0], &modes[2]).set_default("spiral").help("Sets the bucket order mode. default: %default");
     group_tiled.add_option("-o", "--output").action("store").type("string");
     
     group_tiled.add_option("--min-samples").action("store").type("int").set_default(0).help("Sets the minimum sampling. default: %default");
@@ -35,11 +35,6 @@ int main(int argc, char**argv)
     parser.add_option_group(group_tiled);
     
     optparse::Values& parse_options = parser.parse_args(argc, argv);
-
-    std::cout << "width: " << int(parse_options.get("width")) << std::endl;
-    std::cout << "height: " << int(parse_options.get("height")) << std::endl;
-    std::cout << "interactive: " << bool(parse_options.get("interactive")) << std::endl;
-    std::cout << "bucket size: " << int(parse_options.get("bucket_size")) << std::endl;
 
     // render options
     Options options(int(parse_options.get("width")),
@@ -52,7 +47,7 @@ int main(int argc, char**argv)
                     true, // fixed sampling,
                     int(parse_options.get("min_samples")),
                     int(parse_options.get("max_samples")),
-                    int(parse_options.get("nb_threads")),
+                    int(parse_options.get("threads")),
                     std::string(parse_options.get("output")));
 
     // read/construct scene 

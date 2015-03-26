4b268fe08c4a0d8be63ed6762f23141e60ce8571
Benoit <benoit@milk-vfx.com>
2015-03-26 18:31:22 +0000

    Fixed bucket_size option

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char**argv)
     
     optparse::OptionGroup group_tiled = optparse::OptionGroup(parser, "Tiled Renderer Options");
     group_tiled.add_option("--show-window").action("store_true").help("Show a render window.");
-    group_tiled.add_option("--bucket-size").action("int").set_default(64).help("Sets the bucket size. default: %default");
+    group_tiled.add_option("--bucket-size").action("store").type("int").set_default(64).help("Sets the bucket size. default: %default");
     char const* const modes[] = { "topleft", "topright", "bottomleft", "bottomright", "spiral" };
     group_tiled.add_option("-m", "--mode").choices(&modes[0], &modes[5]).set_default("spiral").help("Sets the bucket order mode. default: %default");
     group_tiled.add_option("-o", "--output").action("store").type("string");

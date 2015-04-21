6ec66f0a506e0bc58c2995637f10d5364f1fae7b
Benoit <benoit@milk-vfx.com>
2015-04-21 19:13:29 +0100

    Fixed compilation

--- build.sh
+++ build.sh
@@ -23,9 +23,11 @@ OPTIONS="$OPTIONS -DOSX=1"
 SDL_HEADER_DIR="-I/Library/Frameworks/SDL2.framework/Headers"
 GCC48_LIB_DIR=
 TIFF_HEADER_DIR="-I/Users/benoit/Documents/Prog/libs/include/tiff"
-OPENEXR_LIB_DIR="-L/Users/benoit/Documents/Prog/libs/lib/ -lHalf -lIlmImf"
+OPENEXR_LIB_DIR="-L/Users/benoit/Documents/Prog/libs/lib/"
+OPENEXR_LINK="-lHalf -lIlmImf"
 OPENEXR_HEADER_DIR="-I/Users/benoit/Documents/Prog/libs/include/OpenEXR -I/Users/benoit/Documents/Prog/libs/include/"
-SDL_LIB_DIR="-framework SDL2"
+SDL_LIB_DIR=""
+SDL_LINK="-framework SDL2"
 TIFF_LINK="-ltiff"
 else
 OPTIONS="$OPTIONS -DLINUX=1"
@@ -33,13 +35,15 @@ SDL_HEADER_DIR="-I/milk/code/central/include/SDL/2.0/SDL2/"
 OPENEXR_HEADER_DIR="-I/milk/code/central/include/OpenEXR/2.0.1/"
 GCC48_LIB_DIR="-L/milk/code/central/lib/centos6/gcc-4.8.4/"
 OPENEXR_LIB_DIR="-L/milk/code/central/lib/centos6/gcc-4.4.7/"
-SDL_LIB_DIR="-L/milk/code/central/lib/centos6/gcc-4.4.7/ -lSDL2"
+OPENEXR_LINK="-lHalf -lIlmImf"
+SDL_LIB_DIR="-L/milk/code/central/lib/centos6/gcc-4.4.7/"
+SDL_LINK="-lSDL2"
 TIFF_LINK="-ltiff"
 fi
 
 OPTIONS="$OPTIONS -Wall -Wextra -pedantic -O3 -std=c++11"
 INCLUDE_DIR="-I./ -Idriver -Irenderer -Iintegrator -Isampler -Iutils -I../thirdparty/ctpl -I../thirdparty/optparse ${SDL_HEADER_DIR} ${OPENEXR_HEADER_DIR} ${TIFF_HEADER_DIR}"
-LIB_DIR="${GCC48_LIB_DIR} ${OPENEXR_LIB_DIR} ${SDL_LIB_DIR} ${TIFF_LINK}"
+LIB_DIR="${GCC48_LIB_DIR} ${OPENEXR_LIB_DIR} ${OPENEXR_LINK} ${SDL_LIB_DIR} ${SDL_LINK} ${TIFF_LINK}"
 
 OBJECTS=""
 for FILE in `find . -iname "*.cpp" | sed 's/\.\///g'`; do

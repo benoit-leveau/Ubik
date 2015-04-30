a046686922a8591239ae4011188ceaef3a708fb4
Benoit <benoit@milk-vfx.com>
2015-04-30 16:48:13 +0100

    Added functions to retrieve current and max RAM usage

---
+++ src/memory.hpp
@@ -0,0 +1,15 @@
+/*
+
+***********************************************************
+* © Benoit Leveau, inc. All rights reserved. *
+***********************************************************
+
+*/
+
+#ifndef MEMORY_HPP
+#define MEMORY_HPP
+
+size_t getCurrentRSS();
+size_t getPeakRSS();
+
+#endif // !MEMORY_HPP

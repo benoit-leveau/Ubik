5500aa357da31febe894900d9a809ff1c93c4964
Benoit <benoit@milk-vfx.com>
2019-06-11 16:47:50 +0100

    WIP: still testing the path tracing

--- src/integrator/pathtracer.cpp
+++ src/integrator/pathtracer.cpp
@@ -57,62 +57,73 @@ Color PathTracer::render(size_t x, size_t y, size_t sample)
     Vec3fa color = Vec3fa(0.0f);
     if (ray.geomID != RTC_INVALID_GEOMETRY_ID)
     {
-        if (ray.geomID == 0)
-            return Color(1.0, 0.0, 0.0);
-        else if (ray.geomID == 1)
-            return Color(1.0, 0.0, 0.0);
-        else if (ray.geomID == 2)
-            return Color(0.0, 1.0, 0.0);
-        else if (ray.geomID == 3)
-            return Color(1.0, 1.0, 0.0);
-        else if (ray.geomID == 4)
-            return Color(0.0, 0.0, 1.0);
-        else if (ray.geomID == 5)
-            return Color(1.0, 0.0, 1.0);
-        else if (ray.geomID == 6)
-            return Color(0.0, 1.0, 1.0);
-        else
-            return Color(0.5, 0.5, 0.5);
-        
-        /* interpolate diffuse color */
         Vec3fa diffuse = Vec3fa(1.0f, 1.0f, 1.0f);
+        /*
+          if (ray.geomID == 0)
+            diffuse = Vec3fa(1.0, 0.0, 0.0);
+        else if (ray.geomID == 1)
+            diffuse = Vec3fa(1.0, 0.0, 0.0);
+        else if (ray.geomID == 2)
+            diffuse = Vec3fa(0.0, 1.0, 0.0);
+        else if (ray.geomID == 3)
+            diffuse = Vec3fa(1.0, 1.0, 0.0);
+        else if (ray.geomID == 4)
+            diffuse = Vec3fa(0.0, 0.0, 1.0);
+        else if (ray.geomID == 5)
+            diffuse = Vec3fa(1.0, 0.0, 1.0);
+        else if (ray.geomID == 6)
+            diffuse = Vec3fa(0.0, 1.0, 1.0);
+        */
+
+        /* interpolate diffuse color */
+        /*
         if (ray.geomID > 0)
         {
             unsigned int geomID = ray.geomID;
             {
-                rtcInterpolate(scene->rtc_scene, geomID, ray.primID, ray.u, ray.v, RTC_USER_VERTEX_BUFFER0, &diffuse.x, nullptr, nullptr, 3);
+                rtcInterpolate(scene->rtc_scene, geomID, ray.primID, ray.u, ray.v, RTC_USER_VERTEX_BUFFER0, &diffuse.x, &diffuse.y, &diffuse.z, 3);
             }
             diffuse = 0.5f*diffuse;
-        }
+            }
+        */
 
         color = color + diffuse*0.5f;
-        /*
-        /-* calculate smooth shading normal *-/
-        Vec3fa Ng = normalize(Vec3fa(ray.Ng[0], ray.Ng[1], ray.Ng[2]));
-        Vec3fa lightDir = normalize(Vec3fa(-1,-1,-1));
         
-        /-* initialize shadow ray *-/
+        /* calculate smooth shading normal */
+        Vec3fa Ng = normalize(Vec3fa(ray.Ng[0], ray.Ng[1], ray.Ng[2]));
+        Vec3fa lightDir = normalize(Vec3fa(0-(ray.org[0] + ray.tfar*ray.dir[0]),
+                                           10-(ray.org[1] + ray.tfar*ray.dir[1]),
+                                           0-(ray.org[2] + ray.tfar*ray.dir[2])));
+        
+        /* initialize shadow ray */
         RTCRay shadow;
-        //shadow.org = ray.org + ray.tfar*ray.dir;
-        //shadow.dir = neg(lightDir);
+        shadow.org[0] = ray.org[0] + ray.tfar*ray.dir[0];
+        shadow.org[1] = ray.org[1] + ray.tfar*ray.dir[1];
+        shadow.org[2] = ray.org[2] + ray.tfar*ray.dir[2];
+        shadow.dir[0] = lightDir[0];
+        shadow.dir[1] = lightDir[1];
+        shadow.dir[2] = lightDir[2];
         shadow.tnear = 0.001f;
         shadow.tfar = inf;
-        shadow.geomID = 1;
-        shadow.primID = 0;
+        shadow.geomID = RTC_INVALID_GEOMETRY_ID;
+        shadow.primID = RTC_INVALID_GEOMETRY_ID;
         shadow.mask = -1;
         shadow.time = 0;
 
-        /-* trace shadow ray *-/
-        rtcOccluded(rtc_scene, shadow);
+        /* trace shadow ray */
+        rtcOccluded(scene->rtc_scene, shadow);
 
-        /-* add light contribution *-/
+        /* add light contribution */
         if (shadow.geomID) {
-            Vec3fa r; // = normalize(reflect(ray.dir,Ng));
-            float s = pow(clamp(dot(r,lightDir),0.0f,1.0f),10.0f);
-            float d = clamp(-dot(lightDir,Ng),0.0f,1.0f);
-            color = color + diffuse*d + 0.5f*Vec3fa(s);
+            color.x = 0;
+            color.y = 0;
+            color.z = 0;
+            //Vec3fa r; // = normalize(reflect(ray.dir,Ng));
+            //float s = pow(clamp(dot(r,lightDir),0.0f,1.0f),10.0f);
+            //float d = clamp(-dot(lightDir,Ng),0.0f,1.0f);
+            //color = color + diffuse*d + 0.5f*Vec3fa(s);
         }
-        */
+        
     }
 
     return Color(color.x, color.y, color.z);
